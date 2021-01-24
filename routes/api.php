aa057f791ba6149f4dfd6d855b6e36fc61f71077
lnoueryo <popo62520908@gmail.com>
2021-01-24 19:21:09 +0900

    レビューバックエンド準備完了

--- routes/api.php
+++ routes/api.php
@@ -25,5 +25,6 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 Route::apiResource('bookables', 'Api\BookableController')->only('index', 'show');
 Route::get('bookables/{bookable}/availability', 'Api\BookableAvailabilityController')
     ->name('bookables.availability.show');
-    // /availability?from=2020-09-01&to=2020-09-05
+Route::get('bookables/{bookable}/reviews', 'Api\BookableReviewController')
+    ->name('bookables.reviews.index');
 
