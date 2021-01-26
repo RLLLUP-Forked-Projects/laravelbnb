5a6b0f7f98a5c6003a19f7000151f2bf7294846d
lnoueryo <popo62520908@gmail.com>
2021-01-26 23:23:11 +0900

    Index.vueにrouter-view設置

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -15,14 +15,14 @@
     </head>
     <body>
         <div id="app">
-            {{-- <index></index> --}}
-            <nav class="navbar bg-white border-bottom navbar-light">
+            <index></index>
+            {{--  <nav class="navbar bg-white border-bottom navbar-light">
             <router-link class="navbar-brand mr-auto" :to="{name: 'home'}">LaravelBnb</router-link>
-            {{-- <router-link class="btn nav-button" :to="{name: 'second'}">Second</router-link> --}}
+            <router-link class="btn nav-button" :to="{name: 'second'}">Second</router-link>
         </nav>
         <div class="container mt-4 mb-4 pr-4 pl-4">
             <router-view></router-view>
-        </div>
+        </div>  --}}
 
         </div>
     </body>
