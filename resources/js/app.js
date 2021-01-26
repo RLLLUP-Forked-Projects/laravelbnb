6340eb2aa8adbd6876034c71a19d960201818da8
lnoueryo <popo62520908@gmail.com>
2021-01-26 23:21:41 +0900

    vuex追加

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,5 +1,5 @@
 require('./bootstrap');
-
+window.Vue = require('vue');
 import router from "./routes";
 import VueRouter from "vue-router";
 import Index from "./components/Index"
@@ -7,22 +7,32 @@ import moment from "moment";
 import StarRating from "./shared/components/StarRating";
 import FatalError from "./shared/components/FatalError";
 import ValidationErrors from "./shared/components/ValidationErrors";
+import Success from "./shared/components/Success";
+import Vuex from 'vuex'
+import storeDefinition from './store'
 
-window.Vue = require('vue');
+
 
 
 // Vue.component('example-component', require('./components/ExampleComponent.vue').default);
 // Vue.component('example-2', require('./components/Example2.vue').default);
-
+Vue.use(Vuex)
 Vue.use(VueRouter);
 Vue.filter('fromNow', value => moment(value).fromNow());
 Vue.component('star-rating', StarRating)
 Vue.component('fatal-error', FatalError)
 Vue.component('v-errors', ValidationErrors)
+Vue.component('success', Success)
+
+const store = new Vuex.Store(storeDefinition);
 const app = new Vue({
     el: '#app',
     router,
+    store,
     components: {
         'index': Index
+    },
+    beforeCreate(){
+        this.$store.dispatch('loadStoredState')
     }
 });
