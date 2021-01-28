e6d14ad9984c9b8da5e227b4da2584d0d4bca294
lnoueryo <popo62520908@gmail.com>
2021-01-28 22:09:06 +0900

    カート作成

--- resources/js/bookable/Bookable.vue
+++ resources/js/bookable/Bookable.vue
@@ -55,16 +55,24 @@ export default {
             });
 
     },
-    computed: mapState({
-        lastSearch: 'lastSearch',
-        inBasketAlready(state){
+    computed:{
+    ...mapState({
+            lastSearch: 'lastSearch',
+            inBasketAlready(state){
+                if(null == this.bookable){
+                    return false;
+                }
+
+                return state.basket.items.reduce((result, item) => result || item.bookable.id == this.bookable.id, false);
+            }
+        }),
+        inBasketAlreadyFromGetters(){
             if(null == this.bookable){
                 return false;
             }
-
-            return state.basket.items.reduce((result, item) => result || item.bookable.id == this.bookable.id, false);
+            return this.$store.getters.inBasketAlready(this.bookable.id);
         }
-    }),
+    },
     methods: {
         async checkPrice(hasAvailability){
             if(!hasAvailability){
@@ -79,7 +87,7 @@ export default {
             }
         },
         addToBasket(){
-            this.$store.commit('addToBasket', {
+            this.$store.dispatch('addToBasket', {
                 bookable: this.bookable,
                 price: this.price,
                 dates: this.lastSearch
